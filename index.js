a768f7026a2cbd1be5fa69a992f54829b630d598
Amitai Eldar <amitaieldar@gmail.com>
2022-10-20 20:49:52 +0300

    Add express

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 7071 });
+const express = require('express');
+const { Server } = require('ws');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
 
 const clients = new Map();
 
